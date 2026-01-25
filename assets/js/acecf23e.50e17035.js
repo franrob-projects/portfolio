0987598e76a3e629103b444673a9c20019c9a663
Francis Roberts <111994975+franrob-projects@users.noreply.github.com>
2026-01-25 15:34:42 +0100

    Deploy website - based on 07382429258dbcfa0fc30add999a57ec25fa34cf

---
+++ assets/js/acecf23e.50e17035.js
@@ -0,0 +1,1 @@
+"use strict";(globalThis.webpackChunktechnical_writing_portfolio=globalThis.webpackChunktechnical_writing_portfolio||[]).push([[1903],{1912(o){o.exports=JSON.parse('{"blogBasePath":"/portfolio/blog","blogTitle":"Blog","authorsListPath":"/portfolio/blog/authors"}')}}]);
